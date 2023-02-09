a69d0df70394afa33068d3ffcd934fc616409313
Murat Çelik <muratclik2@gmail.com>
2023-02-10 01:58:28 +0300

    first commit

--- letterCounter/main.c
+++ letterCounter/main.c
@@ -1,14 +1,28 @@
-//
-//  main.c
-//  letterCounter
-//
-//  Created by Murat Çelik on 10.02.2023.
-//
-
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    char metin[100];
+    int harfler[26] = {0};
+
+    printf("Lutfen bir metin giriniz: ");
+    scanf("%[^\n]s", metin);
+    
+
+
+    for (char ww = 'a'; ww <= 'z'; ww++)
+    {
+        char *p = strchr(metin, ww);
+        while (p)
+        {
+            harfler[ww - 'a']++;
+            p = strchr(p + 1, ww);
+        }
+    }
+    for (char ww = 'a'; ww <= 'z'; ww++)
+    {
+        printf("%c harfi %d tane vardır.\n", ww, harfler[ww - 'a']);
+    }
     return 0;
 }
